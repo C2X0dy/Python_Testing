5db18667f51727944190c83e8231fafb4a34987d
Divine <divineyoboukoi2005@gmail.com>
2025-09-29 12:39:13 +0200

    Correction 2

--- server.py
+++ server.py
@@ -26,8 +26,13 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    email = request.form['email']
+    try:
+        club = [club for club in clubs if club['email'] == email][0]
+        return render_template('welcome.html',club=club,competitions=competitions)
+    except IndexError:
+        flash("Sorry, that email wasn't found.")
+        return redirect(url_for('index'))
 
 
 @app.route('/book/<competition>/<club>')
